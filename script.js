5b1fac585fc2fd11abce68123285e4302c43ea63
robertcornelisschuppe <robertcornelis.schuppe@slub-dresden.de>
2026-02-09 16:50:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let storyData = [];
 
 // Standard volume for background music when no "boost" is active
-const BASE_MUSIC_VOL = 0.2; 
+const BASE_MUSIC_VOL = 0.4; 
 
 window.onload = function() {
     const startBtn = document.getElementById('start-btn');
@@ -173,6 +173,7 @@ function parseCSV(csvText) {
     }
     return parsedData;
 }
+
 
 
 
