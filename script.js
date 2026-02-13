7a8482e4615dbcc7467415246957258b2f1dfe8a
robertcornelisschuppe <robertcornelis.schuppe@slub-dresden.de>
2026-02-13 13:10:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let storyData = [];
-const BASE_MUSIC_VOL = 0.2; 
+const BASE_MUSIC_VOL = 0.1; 
 
 window.onload = function() {
     const startBtn = document.getElementById('start-btn');
@@ -197,4 +197,5 @@ function parseCSV(csvText) {
     }
     return parsedData;
 }
+
 
